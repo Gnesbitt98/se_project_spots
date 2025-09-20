77af7233170533d7f9787d9b3870c782c8008707
gagenesbitt <gnesbitt98@gmail.com>
2025-09-20 12:26:50 -0400

    Fix modal UX

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,19 @@ const profileAddModal = document.querySelector("#new-post-modal");
 const profileAddCloseButton = profileAddModal.querySelector(
   ".modal__close-button"
 );
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = profileModal.querySelector("#profile-name-input");
+const profileDescriptionInput = profileModal.querySelector(
+  "#profile-description-input"
+);
+const profileForm = profileModal.querySelector(".modal__form");
+const newPostForm = profileAddModal.querySelector(".modal__form");
+const postImageLink = profileAddModal.querySelector("#card-image-input");
+const postCaption = profileAddModal.querySelector("#card-caption-input");
 
 profileEditButton.addEventListener("click", function () {
+  fillInputFields();
   profileModal.classList.add("modal_is-opened");
 });
 
@@ -22,3 +33,25 @@ profileAddButton.addEventListener("click", function () {
 profileAddCloseButton.addEventListener("click", function () {
   profileAddModal.classList.remove("modal_is-opened");
 });
+
+function fillInputFields() {
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+}
+
+function handleProfileEditSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  profileModal.classList.remove("modal_is-opened");
+}
+
+profileForm.addEventListener("submit", handleProfileEditSubmit);
+
+function handleNewPostSubmit(evt) {
+  evt.preventDefault();
+  console.log(postImageLink.value);
+  console.log(postCaption.value);
+}
+
+newPostForm.addEventListener("submit", handleNewPostSubmit);
